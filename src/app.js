c9e4498b5bb1a019eec3e297fc92f773b3866260
bruninhosrs <brunorodrigues398@gmail.com>
2024-07-15 18:56:29 -0300

    Mudanças no testes para criação de tabelas no BD

--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./Rotas/usuarioRotas');
-const productRoutes = require('./Rotas/produtoRotas');
-const orderRoutes = require('./Rotas/pedidoRotas');
+const userRoutes = require('./Rotas/usuarioRotas'); // Rotas criadas com CRUD basico
+const productRoutes = require('./Rotas/produtoRotas'); // Este arquivo ainda está vazio
+const orderRoutes = require('./Rotas/pedidoRotas'); // Este arquivo ainda está vazio
 
 app.use(express.json()); // Para analisar JSON no corpo da requisição
 
 // Criaçao de rotas, usuarios, pedidos e produtos
 app.use('/users', userRoutes);
-//app.use('/products', productRoutes);
-//app.use('/orders', orderRoutes);
-
-
 
 
 const port = 3000;
-//app.listen(port, () => {
-//    console.log(`Server running on port ${port}`);
-//});
 
  app.listen(port, () => {
      console.log(`Server running at http://localhost:${port}`);
@@ -27,17 +20,27 @@ const port = 3000;
 
 /* TESTES PARA CRIAÇÃO DE TABELAS NO BANCO DE DADOS
 
-const sequelize = require('./config/database'); // Ajuste o caminho conforme necessário
-const User = require('./models/User'); // Ajuste os modelos conforme necessário
-//const Product = require('./models/Product');
-//const Order = require('./models/Order');
+const sequelize = require('./config/database'); // essa linha de código importa o Sequelize configurado no projeto na pasta 'src/config/database.js'
+const User = require('./models/User'); // importa o modelo 'User' definido usando o Sequelize. nesse modelo representa uma tabela no banco de dados. pasta 'src/models/User.js'
+//const Product = require('./models/Product'); // (VAZIA) importa o modelo 'Product' definido usando o Sequelize. nesse modelo representa uma tabela no banco de dados. pasta 'src/models/Product.js'
+//const Order = require('./models/Order'); // (VAZIA) importa o modelo 'Order' definido usando o Sequelize. nesse modelo representa uma tabela no banco de dados. pasta 'src/models/Order.js'
 
 // Sincroniza todos os modelos com o banco de dados
-sequelize.sync({ force: false }) // 'force: true' faria drop das tabelas antes de criar
-  .then(() => {
-    console.log("Tabelas criadas com sucesso.");
-  })
-  .catch(error => {
-    console.error("Erro ao criar tabelas:", error);
-  });
+
+sequelize.showAllSchemas().then((schemas) => {
+  if (schemas.includes('users')) {  // showAllSchemas(): Este método retorna uma lista de todos os esquemas (tabelas) existentes no banco de dados. Assume que 'users' é o nome da tabela para o modelo User
+    console.log('A tabela já existe');
+  } else {
+    sequelize.sync({ force: false }) //comando sincroniza os modelos definidos com o BD. 
+    // Isso significa que o Sequelize vai verificar se existe a tabela desses modelos, caso existá ele não vai exclui-las por causa do parametro { force: false } se trocassemos o 
+    // false pelo 'true' o Sequelize exluirá as tabelas existentes e as recriará, ocasionando a perda de dados existentes.
+
+      .then(() => {
+        console.log("Tabelas criadas com sucesso.");
+      })
+      .catch(error => {
+        console.error("Erro ao criar tabelas:", error);
+      });
+  }
+});
 */
