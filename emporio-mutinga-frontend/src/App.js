5f57d1c01e71363b9ef64933c9bc8a8feb6a9a93
Lanchester <pedrororocha@gmail.com>
2024-08-29 21:19:49 -0300

    Adicionado novo diretório para o Frontend

---
+++ emporio-mutinga-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/products" component={ProductPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
