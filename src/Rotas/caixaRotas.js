a164e0232bd081d76b93a7ed74175ac3b1d5fc6d
bruninhosrs <brunorodrigues398@gmail.com>
2024-10-08 10:52:28 -0300

    Modificações em gestão de caixas

--- src/Rotas/caixaRotas.js
+++ src/Rotas/caixaRotas.js
@@ -5,11 +5,11 @@ const CashRegisterController = require('../controllers/CashRegisterController');
 const { authenticateToken, authorizeRole } = require('../middleware/authenticateToken');
 
 // Operações de caixa para um caixa específico (registerNumber)
-router.post('/open', authenticateToken, authorizeRole(['gerente', 'super-admin']), CashRegisterController.openCashRegister);
-router.post('/close/:registerNumber', authenticateToken, authorizeRole(['gerente', 'super-admin']), CashRegisterController.closeCashRegister);
-router.post('/entry/:registerNumber', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.addEntry);
-router.post('/exit/:registerNumber', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.addExit);
-router.post('/cashout/:registerNumber', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.cashOut);
-router.get('/balance/:registerNumber', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.getBalance);
+router.get('/', CashRegisterController.listAllCash);
+router.post('/open', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.openCashRegister);
+router.post('/close', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.closeCashRegister);
+router.post('/withdraw', authenticateToken, authorizeRole(['sub-gerente', 'gerente', 'super-admin']), CashRegisterController.withdrawFromCashRegister); // Sangria de caixa
+router.get('/balance/:registerNumber', authenticateToken, CashRegisterController.getCashRegisterBalance);
+router.get('/sales/:registerNumber', authenticateToken, CashRegisterController.getSalesByMethod);
 
 module.exports = router;
